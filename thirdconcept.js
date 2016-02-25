7fdde6b45be666fb6600c0228892bbf8ab2e69f1
Laura Stupi <laurastupi@Lauras-MacBook-Air-3.local>
2016-02-24 20:48:40 -0800

    hello3

--- thirdconcept.js
+++ thirdconcept.js
@@ -20,29 +20,29 @@ $( document ).ready(function(){
 
   $('.xo').hover(function() {
     $('.xolinks').slideDown();
-    $('html').css('background-image', 'url("./imagescopy/IMG_7176.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_7176.jpg")');
 
   });
 
   $('.aid').hover(function(){
-    $('html').css('background-image', 'url("./imagescopy/IMG_6227.jpeg")');
-    // $('html').animate({opacity: 0}, 0).css("background-image", "url('./imagescopy/IMG_6227.jpeg')").animate({opacity: 1}, 2500);
+    $('html').css('background-image', 'url("imagescopy/IMG_6227.jpeg")');
+    // $('html').animate({opacity: 0}, 0).css("background-image", "url('imagescopy/IMG_6227.jpeg')").animate({opacity: 1}, 2500);
   });
 
   $('.eli').hover(function(){
-    $('html').css('background-image', 'url("./imagescopy/IMG_5310.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_5310.jpg")');
   });
 
   $('.john').hover(function(){
-    $('html').css('background-image', 'url("./imagescopy/IMG_7210.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_7210.jpg")');
   });
 
   $('.baby').hover(function(){
-    $('html').css('background-image', 'url("./imagescopy/IMG_6866.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_6866.jpg")');
   });
 
   $('.babygene').hover(function(){
-    $('html').css('background-image', 'url("./imagescopy/IMG_1718.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_1718.jpg")');
   });
 
 })
