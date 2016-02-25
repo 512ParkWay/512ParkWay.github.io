c2ee3bc3a4c597b448def7103771e432254ae241
Laura Stupi <laurastupi@Lauras-MacBook-Air-3.local>
2016-02-24 20:50:00 -0800

    hello4

--- thirdconcept.js
+++ thirdconcept.js
@@ -34,7 +34,7 @@ $( document ).ready(function(){
   });
 
   $('.john').hover(function(){
-    $('html').css('background-image', 'url("imagescopy/IMG_7210.jpg")');
+    $('html').css('background-image', 'url("imagescopy/IMG_7210.JPG")');
   });
 
   $('.baby').hover(function(){
